c0862ec06f9d1b8924d1ea95988f236d6dcc45ed
Matias Bubello <matt.bubello@gmail.com>
2023-12-12 17:54:55 -0300

    styles, ignore, fonts, rest of the landing, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+         'primary': '#95000B',
+         'secondary': '#DD0616',
+         'tertiary': '#C7520C',
+         'quaternary': '#FAEA00',
+         'quinary': '#322928',
+         'senary': '#E2E0D7',
+      },
+    },
+  },
+  plugins: [],
+}
+
